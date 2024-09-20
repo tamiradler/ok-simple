dd253af2143183a92fa0d8263abd8b45a353b9da
Tamir Adler <tamir@clarifruit.com>
2024-09-20 10:59:09 +0300

    adding loading

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+      <p className="text-white text-2xl">Loading...</p>
+    </div>
+  );
+}
