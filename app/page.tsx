170f7ab32c69d07e7f9ab9949ae9a15e1daf4520
Tamir Adler <tamir@clarifruit.com>
2024-09-20 11:02:31 +0300

    adding count

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Link from "next/link";
+import { prisma } from "@/prisma/prisma";
 
 export default async function Home() {
+  const count = await prisma.order.count();
+
   return (
-    <>Home</>
+    <>There are {count} orders currently</>
   );
 }
