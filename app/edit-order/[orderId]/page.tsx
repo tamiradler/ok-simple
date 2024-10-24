5d0221dd321688d792ac91ffea9f72c7c9ba6182
Tamir Adler <tamir@clarifruit.com>
2024-10-24 22:40:32 +0300

    use the new update support

--- app/edit-order/[orderId]/page.tsx
+++ app/edit-order/[orderId]/page.tsx
@@ -1,4 +1,6 @@
+import { getProductGroups, getZones } from "@/app/actions/catalog-data";
 import { getOrder } from "@/app/actions/orders";
+import { NewOrder } from "@/app/new-order/new-order";
 
 export default async function Page({
   params,
@@ -6,5 +8,26 @@ export default async function Page({
   params: { orderId: string };
 }) {
   const order = await getOrder(Number(params.orderId));
-  return <><h1>Order id {order?.incrementalId}</h1></>;
+  const [productGroups, zones] = await Promise.all([
+    getProductGroups(),
+    getZones(),
+  ]);
+  return (
+    <>
+      <h1>Order id {order?.incrementalId}</h1>
+      <NewOrder
+        productGroups={productGroups}
+        zones={zones}
+        order={{
+          cityId: order?.cityid || "",
+          corporateId: order?.corporateId || 0,
+          customerName: order?.customerName || "",
+          date: order?.date || new Date(),
+          discount: order?.discount.toNumber() || 0,
+          price: order?.price.toNumber() || 0,
+          productTypeId: order?.productTypeid || "",
+        }}
+      ></NewOrder>
+    </>
+  );
 }
