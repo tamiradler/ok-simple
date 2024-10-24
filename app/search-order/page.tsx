2cf3d33bc6109cbbfcc995e3cb2e51426e46810d
Tamir Adler <tamir@clarifruit.com>
2024-10-25 00:35:01 +0300

    mobile responsive

--- app/search-order/page.tsx
+++ app/search-order/page.tsx
@@ -20,45 +20,47 @@ export default async function Page({
     <div className="container mx-auto p-4">
       Total orders: {count}
       <div className="flex flex-col">
-        <div className="flex bg-gray-200 p-2 font-bold">
-          <div className="flex-1">ID</div>
-          <div className="flex-1">תאריך</div>
-          <div className="flex-1">עיר</div>
-          <div className="flex-1">מוצר</div>
-          <div className="flex-1">שם לקוח</div>
-          <div className="flex-1">עוסק מורשה</div>
-          <div className="flex-1">מחיר</div>
-          <div className="flex-1">הנחה</div>
-          <div className="flex-1">מחיר סופי</div>
-        </div>
-        {orders.map((order) => (
-          <Link
-            href={{
-              pathname: `/edit-order/${order.incrementalId}`,
-            }}
-          >
-            <div key={order.id} className="flex p-2 border-b">
-              <div className="flex-1">{order.incrementalId}</div>
-              <div className="flex-1">{order.date.toLocaleDateString()}</div>
-              <div className="flex-1">{order.city.name}</div>
-              <div className="flex-1">{order.productType.name}</div>
-              <div className="flex-1">{order.customerName}</div>
-              <div className="flex-1">{order.corporateId}</div>
-              <div className="flex-1">{order.price.toFixed(2)}</div>
-              <div className="flex-1">{order.discount.toFixed(2)}</div>
-              <div className="flex-1">
-                {order.price
-                  .times(
-                    order.discount
-                      .times(1 / 100)
-                      .minus(1)
-                      .times(-1)
-                  )
-                  .toFixed(2)}
+        <div className="overflow-x-auto">
+          <div className="min-w-[1000px] flex bg-gray-200 p-2 font-bold">
+            <div className="flex-1">ID</div>
+            <div className="flex-1">תאריך</div>
+            <div className="flex-1">עיר</div>
+            <div className="flex-1">מוצר</div>
+            <div className="flex-1">שם לקוח</div>
+            <div className="flex-1">עוסק מורשה</div>
+            <div className="flex-1">מחיר</div>
+            <div className="flex-1">הנחה</div>
+            <div className="flex-1">מחיר סופי</div>
+          </div>
+          {orders.map((order) => (
+            <Link
+              href={{
+                pathname: `/edit-order/${order.incrementalId}`,
+              }}
+            >
+              <div key={order.id} className="min-w-[1000px] flex p-2 border-b">
+                <div className="flex-1">{order.incrementalId}</div>
+                <div className="flex-1">{order.date.toLocaleDateString()}</div>
+                <div className="flex-1">{order.city.name}</div>
+                <div className="flex-1">{order.productType.name}</div>
+                <div className="flex-1">{order.customerName}</div>
+                <div className="flex-1">{order.corporateId}</div>
+                <div className="flex-1">{order.price.toFixed(2)}</div>
+                <div className="flex-1">{order.discount.toFixed(2)}</div>
+                <div className="flex-1">
+                  {order.price
+                    .times(
+                      order.discount
+                        .times(1 / 100)
+                        .minus(1)
+                        .times(-1)
+                    )
+                    .toFixed(2)}
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
+        </div>
       </div>
       <Link
         href={{
