43e0fffc2b4e2189581302d07fea435481d5e296
Tamir Adler <tamir@clarifruit.com>
2024-10-26 20:27:12 +0300

    make the text in Hebrew for the new order

--- app/new-order/new-order.tsx
+++ app/new-order/new-order.tsx
@@ -85,23 +85,31 @@ export function NewOrder({
       return;
     }
     if (!selectedProduct) {
-      alert("City not selected");
+      alert("Product not selected");
       return;
     }
-    const regex = /^\d{9}$/;
-    if (!corporateId || regex.test(corporateId)) {
-      alert("corporateId not valid");
+    const regex = /^[0-9]{8,9}$/;
+    if (!corporateId || !regex.test(corporateId.trim())) {
+      alert("מספר ח.פ/עוסק מורשה לא תקין, צריך ליהיות באורך 8 עד 9 ספרות");
       return;
     }
-    if (!customerName) {
-      alert("customerName not selected");
+    if (!customerName || !customerName.trim()) {
+      alert("שם לקוח ריק");
+      return;
+    }
+    if (discount > 100 || discount < 0) {
+      alert("הנחה לא תקינה, צריכה ליהיות בין 0 ל 100.");
+      return;
+    }
+    if (price < 0) {
+      alert("מכיר לא תקין, צריך ליהיות גדול מ- 0.");
       return;
     }
     setIsLoading(true);
     const orderToPersist = {
       cityId: selectedCity.id,
-      corporateId: Number(corporateId),
-      customerName,
+      corporateId: Number(corporateId.trim()),
+      customerName: customerName.trim(),
       date: order?.date || new Date(),
       discount,
       price,
@@ -113,127 +121,132 @@ export function NewOrder({
     } else {
       promise = createNewOrder(orderToPersist);
     }
-    promise
-      .then(() => router.push("/"))
-      .finally(() => setIsLoading(false));
+    promise.then(() => router.push("/")).finally(() => setIsLoading(false));
   };
 
   return (
-    <>
-      <label className="block text-sm font-medium text-gray-700">
-        Select Product Group
-      </label>
-      <select
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        onChange={handleGroupChange}
-        value={selectedGroup?.id || ""}
-      >
-        <option value="">Select Group</option>
-        {productGroups.map((group) => (
-          <option key={group.id} value={group.id}>
-            {group.name}
-          </option>
-        ))}
-      </select>
-      <label className="block text-sm font-medium text-gray-700">
-        Select Product Type
-      </label>
-      <select
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        onChange={handleProductChange}
-        value={selectedProduct?.id || ""}
-      >
-        <option value="">Select Product</option>
-        {selectedGroup?.productTypes.map((product) => (
-          <option key={product.id} value={product.id}>
-            {product.name}
-          </option>
-        ))}
-      </select>
-      <label className="block text-sm font-medium text-gray-700">
-        Select Zone
-      </label>
-      <select
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        onChange={handleZoneChange}
-        value={selectedZone?.id || ""}
-      >
-        <option value="">Select Zone</option>
-        {zones.map((zone) => (
-          <option key={zone.id} value={zone.id}>
-            {zone.name}
-          </option>
-        ))}
-      </select>
-      <label className="block text-sm font-medium text-gray-700">
-        Select City
-      </label>
-      <select
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        onChange={handleCityChange}
-        value={selectedCity?.id || ""}
-      >
-        <option value="">Select City</option>
-        {selectedZone?.cities.map((city) => (
-          <option key={city.id} value={city.id}>
-            {city.name}
-          </option>
-        ))}
-      </select>
-      <label className="block text-sm font-medium text-gray-700">
-        Customer Name
-      </label>
-      <input
-        type="text"
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-        value={customerName}
-        onChange={(e) => setCustomerName(e.target.value)}
-        placeholder="Enter customer name"
-      />
+    <div className="flex justify-end">
+      <div>
+        <label className="block text-sm font-medium text-gray-700 text-right">
+          בחר מחלקת מוצר
+        </label>
+        <select
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-right"
+          onChange={handleGroupChange}
+          value={selectedGroup?.id || ""}
+        >
+          <option value="">מחלקת מוצר</option>
+          {productGroups.map((group) => (
+            <option key={group.id} value={group.id}>
+              {group.name}
+            </option>
+          ))}
+        </select>
 
-      <label className="block text-sm font-medium text-gray-700">
-        Corporate ID
-      </label>
-      <input
-        type="text"
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-        value={corporateId}
-        onChange={(e) => setCorporateId(e.target.value)}
-        placeholder="Enter corporate ID"
-      />
+        <label className="block text-sm font-medium text-gray-700 text-right pt-5">
+          סוג מוצר
+        </label>
+        <select
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-right"
+          onChange={handleProductChange}
+          value={selectedProduct?.id || ""}
+        >
+          <option value="">בחר סוג מוצר</option>
+          {selectedGroup?.productTypes.map((product) => (
+            <option key={product.id} value={product.id}>
+              {product.name}
+            </option>
+          ))}
+        </select>
 
-      <label className="block text-sm font-medium text-gray-700">Price</label>
-      <input
-        type="number"
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-        value={price}
-        onChange={(e) => setPrice(parseFloat(e.target.value))}
-        placeholder="Enter price"
-      />
+        <label className="block text-sm font-medium text-gray-700 text-right pt-5">
+          אזור
+        </label>
+        <select
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-right"
+          onChange={handleZoneChange}
+          value={selectedZone?.id || ""}
+        >
+          <option value="">בחר אזור</option>
+          {zones.map((zone) => (
+            <option key={zone.id} value={zone.id}>
+              {zone.name}
+            </option>
+          ))}
+        </select>
 
-      <label className="block text-sm font-medium text-gray-700">
-        Discount
-      </label>
-      <input
-        type="number"
-        className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-        value={discount}
-        onChange={(e) => setDiscount(parseFloat(e.target.value))}
-        placeholder="Enter discount"
-      />
-      <button
-        type="button"
-        className="w-48 bg-blue-500 text-white mt-5 py-2 px-4 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-        onClick={handleSubmit}
-      >
-        Submit
-      </button>
+        <label className="block text-sm font-medium text-gray-700 text-right pt-5">
+          עיר
+        </label>
+        <select
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-right"
+          onChange={handleCityChange}
+          value={selectedCity?.id || ""}
+        >
+          <option value="">בחר עיר</option>
+          {selectedZone?.cities.map((city) => (
+            <option key={city.id} value={city.id}>
+              {city.name}
+            </option>
+          ))}
+        </select>
 
-      {isLoading && (
-        <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <p className="text-white text-2xl">Saving...</p>
-        </div>
-      )}
-    </>
+        <label className="block text-sm font-medium text-gray-700 text-right pt-5">
+          שם לקוח
+        </label>
+        <input
+          type="text"
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm text-right"
+          value={customerName}
+          onChange={(e) => setCustomerName(e.target.value)}
+        />
+
+        <label className="block text-sm font-medium text-gray-700 text-right pt-5">
+          מספר ח.פ/עוסק מורשה
+        </label>
+        <input
+          type="text"
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm text-right"
+          value={corporateId}
+          onChange={(e) => setCorporateId(e.target.value)}
+        />
+
+        <label className="block text-sm font-medium text-gray-700 text-right pt-5">
+          עלות מוצר
+        </label>
+        <input
+          type="number"
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm text-right"
+          value={price}
+          onChange={(e) => setPrice(parseFloat(e.target.value))}
+          placeholder="Enter price"
+        />
+
+        <label className="block text-sm font-medium text-gray-700 text-right pt-5">
+          אחוז הנחה
+        </label>
+        <input
+          type="number"
+          className="mt-1 block w-48 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm text-right"
+          value={discount}
+          onChange={(e) => setDiscount(parseFloat(e.target.value))}
+          placeholder="Enter discount"
+        />
+
+        <button
+          type="button"
+          className="w-48 bg-blue-500 text-white mt-5 py-2 px-4 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          onClick={handleSubmit}
+        >
+          שמור
+        </button>
+
+        {isLoading && (
+          <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+            <p className="text-white text-2xl">Saving...</p>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
